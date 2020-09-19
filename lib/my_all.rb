92ab82c3930d33d6583da2ff11598cdb6562c581
Nick Nobile <nickcnobile@gmail.com>
2020-09-19 01:09:50 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,9 +1,9 @@
 def my_all?(collection)
-  i = 0
+  counter = 0
   block_return_values = []
-  while i < collection.length
-    block_return_values << yield(collection[i])
-    i = i + 1
+  while counter < collection.length
+    block_return_values << yield(collection[counter])
+    counter += 1
   end
 
   if block_return_values.include?(false)
