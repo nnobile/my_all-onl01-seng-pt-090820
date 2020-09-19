6290178268a71b174c0ee6970dcff9fcc4021dec
Nick Nobile <nickcnobile@gmail.com>
2020-09-19 01:08:38 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,11 +5,9 @@ def my_all?(collection)
     block_return_values << yield(collection[counter])
     counter += 1
   end
-end
 
 if block_return_values.include?(false)
   false
 else
   true
-end
 end
