fa63971d758cb1869770c6d3ef238f0974ea26d8
Nick Nobile <nickcnobile@gmail.com>
2020-09-19 01:01:37 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def my_all?(collection)
   counter = 0
-  while counter < collection.length
+  while counter < collection.length yield(collection[counter])
     counter += 1
   end
 end
